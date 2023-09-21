aa40bcc372a1ddcbf179365c683a49b72fbc33ba
Danrley Lima <danrleylac@gmail.com>
2023-09-20 23:00:51 -0300

    feat: bottomNavigationBar

--- lib/styles.dart
+++ lib/styles.dart
@@ -1,5 +1,6 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 const lightColorScheme = ColorScheme(
   brightness: Brightness.light,
