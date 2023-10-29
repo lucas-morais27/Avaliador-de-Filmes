3fe5255970ae3fb87283c448f3cf4e111053d5b1
Danrley Lima <danrleylac@gmail.com>
2023-10-29 12:31:23 -0300

    feat: tela de login sem lógica

--- lib/screens/incial_page.dart
+++ lib/screens/incial_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:und1_mobile/models/producao_model.dart';
+import 'package:und1_mobile/screens/login_page.dart';
 import 'package:und1_mobile/screens/menu_navegacao_page.dart';
 
 class PaginaInicial extends StatelessWidget {
@@ -8,8 +9,14 @@ class PaginaInicial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider(create: (context) => ProducaoModel())
-    ], child: const MenuNavegacao());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProducaoModel(),
+        )
+      ],
+      // child: const MenuNavegacao(),
+      child: const LoginPage(),
+    );
   }
 }
