fc87ea574fe3f8165ebc17a929101d13c159dbae
Ian Barreto <ian.barreto.700@ufrn.edu.br>
2023-10-31 15:28:28 -0300

    Ajustes na tela de login

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:und1_mobile/models/usuario.dart';
 import 'package:und1_mobile/utils/app_routes.dart';
 
+import '../styles.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -12,6 +14,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usuarioController = TextEditingController();
   final TextEditingController _senhaController = TextEditingController();
+  var _senhaVisivel = false;
 
   Future<void> _showLoginErrorDialog(BuildContext context) async {
     return showDialog(
@@ -45,110 +48,137 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     var cores = Theme.of(context).colorScheme;
+    ButtonStyle _buttonStyle = ButtonStyle(
+      padding: const MaterialStatePropertyAll<EdgeInsets>(
+          EdgeInsets.all(12)),
+      shape: MaterialStatePropertyAll<
+          RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+              borderRadius:
+              BorderRadius.circular(32))),
+      backgroundColor:
+      MaterialStatePropertyAll<Color>(
+          cores.primaryContainer),
+      foregroundColor:
+      MaterialStatePropertyAll<Color>(
+          cores.onPrimaryContainer),
+      textStyle:
+      const MaterialStatePropertyAll<TextStyle>(
+          estiloSubTitulo3),
+    );
 
     return Scaffold(
-      backgroundColor: cores.onSecondaryContainer,
+      backgroundColor: cores.onPrimaryContainer,
       appBar: AppBar(
         title: const Text(
           'Login',
-          style: TextStyle(
-            fontSize: 24,
-          ),
+          style: estiloTitulo3,
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const SizedBox(height: 16),
-              Image.asset(
-                "assets/clapper.png",
-                height: 290,
-              ),
-              const SizedBox(height: 32),
-              TextFormField(
-                style: TextStyle(color: cores.onSecondary, fontSize: 18),
-                controller: _usuarioController,
-                decoration: InputDecoration(
-                  labelText: 'Usuário',
-                  labelStyle: TextStyle(color: cores.onSecondary),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: cores.primary,
-                      width: 2.0,
-                    ),
+      body: CustomScrollView(
+        scrollDirection: Axis.vertical,
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  const SizedBox(height: 16),
+                  Image.asset(
+                    "assets/clapper.png",
+                    height: 150,
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: cores.primary,
-                      width: 2.0,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                style: TextStyle(color: cores.onSecondary, fontSize: 18),
-                controller: _senhaController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  labelStyle: TextStyle(color: cores.onSecondary),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: cores.primary,
-                      width: 2.0,
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: cores.primary,
-                      width: 2.0,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24),
-              ElevatedButton(
-                onPressed: () async {
-                  String email = _usuarioController.text;
-                  String senha = _senhaController.text;
-
-                  try {
-                    Usuario? usuarioLogado = await Usuario.login(email, senha);
-                    if (usuarioLogado != null) {
-                      if (!context.mounted) {
-                        return;
+                  const SizedBox(height: 32),
+                  TextFormField(
+                    controller: _usuarioController,
+                    style: TextStyle(color: cores.onSecondaryContainer),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, insira um email.';
                       }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      icon: Icon(Icons.mail),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: _senhaController,
+                    obscureText: !_senhaVisivel,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, insira uma senha.';
+                      } else if (value.length < 8) {
+                        return 'A senha deve ter pelo menos 8 caracteres.';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Senha',
+                        hintText: 'Pelo menos 8 caracteres',
+                        hintStyle: estiloCorpoTexto3,
+                        icon: const Icon(Icons.password),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _senhaVisivel = !_senhaVisivel;
+                            });
+                          },
+                          icon: _senhaVisivel
+                              ? const Icon(Icons.visibility)
+                              : const Icon(Icons.visibility_off),
+                        )),
+                  ),
+                  const Expanded(child: SizedBox(height: 0,)),
+                  ElevatedButton(
+                      onPressed: () async {
+                        String email = _usuarioController.text;
+                        String senha = _senhaController.text;
 
-                      // Passei o usuário pra frente, vai que precisa em outro lugar
-                      Navigator.of(context)
-                          .pushNamed(AppRoutes.HOME, arguments: usuarioLogado);
-                    }
-                  } catch (e) {
-                    _showLoginErrorDialog(context);
-                  }
-                },
-                child: const Text(
-                  'Entrar',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
+                        try {
+                          Usuario? usuarioLogado = await Usuario.login(email, senha);
+                          if (usuarioLogado != null) {
+                            if (!context.mounted) {
+                              return;
+                            }
+
+                            // Passei o usuário pra frente, vai que precisa em outro lugar
+                            Navigator.of(context)
+                                .pushNamed(AppRoutes.HOME, arguments: usuarioLogado);
+                          }
+                        } catch (e) {
+                          _showLoginErrorDialog(context);
+                        }
+                      },
+                      child: const Text(
+                        'Entrar',
+                      ),
+                      style: _buttonStyle
+                  ),
+                  SizedBox(height: 16,),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(AppRoutes.CADASTRO);
+                    },
+                    style: _buttonStyle,
+                    child: const Text(
+                      'Cadastre-se',
+                    ),
+                  ),
+                ],
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed(AppRoutes.CADASTRO);
-                },
-                child: const Text(
-                  'Cadastre-se',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
